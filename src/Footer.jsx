27f9654dadd89bb9b9de106e53e760b75cbf3f50
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-25 00:53:04 +0630

    Add footer

---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="mb-2 lg:mb-0 lg:fixed lg:bottom-2 lg:right-3">
+      <p className="text-[#21092f]">
+        coded by{" "}
+        <a
+          className=" hover:font-bold"
+          href="https://portfolio-neew.netlify.app/"
+          target={"_blank"}
+        >
+          Nann Ei Ei Win
+        </a>
+        , 2023
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import FormState from "./FormState";
 import FormCompleteState from "./FormCompleteState";
-
+import Footer from "./Footer";
 function FormComponent() {
   return (
-    <div className="FormComponent flex justify-center items-center mt-14 md:mt-32 lg:mt-0 lg:ml-60 lg:mr-10 xl:mx-80 ">
+    <div className="FormComponent flex justify-center flex-col items-center mt-14 md:mt-32 lg:mt-0 lg:ml-60 lg:mr-10 xl:mx-80 ">
       <FormState />
       <FormCompleteState />
+      <Footer />
     </div>
   );
 }
